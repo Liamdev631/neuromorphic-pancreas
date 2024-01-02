ab4c9e703e2037aa1ffe4495b1edb76e1603cb8c
Liam Bury <liambury@outlook.com>
2024-01-02 18:25:34 -0500

    Corrected dataset times

--- glucose_dataset.py
+++ glucose_dataset.py
@@ -4,14 +4,15 @@ import pandas as pd
 import torch
 
 class GlucoseDataset(Dataset):
-    def __init__(self, zip_file_path, sub_directory, sample_length=16, transform=None):
+    def __init__(self, zip_file_path, sub_directory, input_length=16, output_length=8, transform=None):
         self.zip_file = ZipFile(zip_file_path, 'r')
         self.file_list = self.zip_file.namelist()  # Get list of files in the zip
         # Remove all files not in the subdirectory
         self.file_list = [file for file in self.file_list if file.startswith(sub_directory)]
         # Only load xls and xlsx files
         self.file_list = [file for file in self.file_list if file.endswith('.xls') or file.endswith('.xlsx')]
-        self.sample_length = sample_length
+        self.input_length = input_length
+        self.output_length = output_length
         self.transform = transform
 
         # Fetch the min and max values of the glucose values
@@ -22,18 +23,18 @@ class GlucoseDataset(Dataset):
             self.min = min(self.min, glucose.min().item())
             self.max = max(self.max, glucose.max().item())
 
-        self.sample_interval = 0.25 * 60 # 15 minutes
+        self.sample_interval: float = 0.25 * 60 * 60 # 15 minutes in seconds
 
     def __len__(self):
         return len(self.file_list)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> torch.Tensor:
         glucose = self._fetch_raw_glucose_data(self.file_list[idx])
         normalized_glucose = (glucose - self.min) / (self.max - self.min)
         return normalized_glucose
         
-    def _fetch_raw_glucose_data(self, file: str) -> torch.Tensor:
-        with self.zip_file.open(file) as file:
+    def _fetch_raw_glucose_data(self, filename: str) -> torch.Tensor:
+        with self.zip_file.open(filename) as file:
             # Read the xlsx file
             df = pd.read_excel(file)
             glucose = torch.tensor(df.iloc[:, 1].values, dtype=torch.float32)
